96ecb93ba0a40902d12cd1ac1ea9d902eb1612cd
eternasuno <22316214+eternasuno@users.noreply.github.com>
2026-05-19 14:55:23 +0900

    feat: add opencode setting

--- module/common/cli/opencode.nix
+++ module/common/cli/opencode.nix
@@ -7,6 +7,11 @@ in {
   home-manager.users.${username} = {
     programs.opencode = {
       enable = true;
+      settings = {
+        "$schema" = "https://opencode.ai/config.json";
+        plugin = ["superpowers@git+https://github.com/obra/superpowers.git"];
+        lsp = true;
+      };
     };
   };
 }

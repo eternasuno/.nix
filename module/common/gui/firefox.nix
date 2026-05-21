596c6c899cdf128e14e7bc2d13753480c911f17c
eternasuno <22316214+eternasuno@users.noreply.github.com>
2026-05-22 08:45:09 +0900

    feat: update firefox config
    Signed-off-by: eternasuno <22316214+eternasuno@users.noreply.github.com>

--- module/common/gui/firefox.nix
+++ module/common/gui/firefox.nix
@@ -28,37 +28,93 @@ in {
       policies = {
         AutofillAddressEnabled = false;
         AutofillCreditCardEnabled = false;
-        DisableFirefoxAccounts = true;
         DisableAppUpdate = true;
+        DisableFirefoxAccounts = true;
         DisableTelemetry = true;
         DontCheckDefaultBrowser = true;
+
         ExtensionSettings = {
+          "@testpilot-containers" = {
+            installation_mode = "force_installed";
+            install_url = "https://addons.mozilla.org/firefox/downloads/latest/multi-account-containers/latest.xpi";
+          };
           "extension@tabliss.io" = {
             installation_mode = "force_installed";
             install_url = "https://addons.mozilla.org/firefox/downloads/latest/extension@tabliss.io/latest.xpi";
+          };
+          "firefox@tampermonkey.net" = {
+            installation_mode = "force_installed";
+            install_url = "https://addons.mozilla.org/firefox/downloads/latest/tampermonkey/latest.xpi";
           };
           "uBlock0@raymondhill.net" = {
             installation_mode = "force_installed";
             install_url = "https://addons.mozilla.org/firefox/downloads/latest/uBlock0@raymondhill.net/latest.xpi";
           };
         };
+
         RequestedLocales = ["zh-CN"];
       };
+
       profiles.default = {
         id = 0;
         name = "default";
         isDefault = true;
+
         search = {
           force = true;
           default = "ddg";
         };
+
         settings = {
+          # locale
           "intl.locale.requested" = "zh-CN";
-          "toolkit.legacyUserProfileCustomizations.stylesheets" = true;
-          "svg.context-properties.content.enabled" = true;
+
+          # fonts
           "font.size.variable.x-western" = 18;
           "font.size.monospace.x-western" = 16;
           "font.minimum-size.x-western" = 14;
+
+          # legacy
+          "toolkit.legacyUserProfileCustomizations.stylesheets" = true;
+          "svg.context-properties.content.enabled" = true;
+
+          # privacy / security
+          "dom.security.https_only_mode" = true;
+          "privacy.bounceTrackingProtection.mode" = 1;
+          "privacy.fingerprintingProtection" = true;
+          "privacy.globalprivacycontrol.enabled" = true;
+          "privacy.query_stripping.enabled" = true;
+          "privacy.query_stripping.enabled.pbmode" = true;
+          "privacy.sanitize.sanitizeOnShutdown" = true;
+          "privacy.trackingprotection.enabled" = true;
+          "privacy.trackingprotection.emailtracking.enabled" = true;
+          "privacy.trackingprotection.socialtracking.enabled" = true;
+          "privacy.userContext.enabled" = true;
+          "privacy.userContext.ui.enabled" = true;
+          "signon.rememberSignons" = false;
+
+          # network / dns
+          "network.dns.disablePrefetch" = true;
+          "network.http.speculative-parallel-limit" = 0;
+          "network.prefetch-next" = false;
+
+          # ui
+          "browser.ctrlTab.sortByRecentlyUsed" = true;
+          "browser.tabs.inTitlebar" = 1;
+          "browser.toolbars.bookmarks.visibility" = "never";
+          "browser.translations.automaticallyPopup" = false;
+          "sidebar.revamp" = true;
+          "sidebar.verticalTabs" = true;
+
+          # devtools
+          "devtools.chrome.enabled" = true;
+          "devtools.debugger.remote-enabled" = true;
+
+          # misc
+          "browser.ml.chat.footerBadge" = false;
+          "browser.ml.chat.shortcuts" = false;
+          "layout.css.has-selector.enabled" = true;
+          "layout.spellcheckDefault" = 0;
         };
       };
     };

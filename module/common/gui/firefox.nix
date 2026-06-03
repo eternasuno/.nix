bd417d8069e316ff99937fef54580e972e08cefa
eternasuno <22316214+eternasuno@users.noreply.github.com>
2026-06-03 11:30:26 +0900

    feat: add firefox vim plugin
    Signed-off-by: eternasuno <22316214+eternasuno@users.noreply.github.com>

--- module/common/gui/firefox.nix
+++ module/common/gui/firefox.nix
@@ -50,6 +50,10 @@ in {
             installation_mode = "force_installed";
             install_url = "https://addons.mozilla.org/firefox/downloads/latest/uBlock0@raymondhill.net/latest.xpi";
           };
+          "{d7742d87-e61d-4b78-b8a1-b469842139fa}" = {
+            installation_mode = "force_installed";
+            install_url = "https://addons.mozilla.org/firefox/downloads/latest/vimium-ff/latest.xpi";
+          };
         };
 
         RequestedLocales = ["zh-CN"];

f7528a4e66017ae4846d760b6a39285b63e3a0a8
eternasuno <22316214+eternasuno@users.noreply.github.com>
2026-07-17 21:37:43 +0900

    feat: rm purescript
    Signed-off-by: eternasuno <22316214+eternasuno@users.noreply.github.com>

--- hosts/MacBook-Air/default.nix
+++ hosts/MacBook-Air/default.nix
@@ -17,9 +17,6 @@ in {
     system = "aarch64-darwin";
     specialArgs = {inherit inputs vars;};
     modules = [
-      {
-        nixpkgs.overlays = [inputs.purescript-overlay.overlays.default];
-      }
       ./configuration.nix
       home-manager.darwinModules.home-manager
     ];
